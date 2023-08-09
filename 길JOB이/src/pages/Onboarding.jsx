61d907413b01883b30fb2f32d7e26c05cc3be783
yueunfive <122276414+yueunfive@users.noreply.github.com>
2023-08-09 11:28:40 +0900

    동적 클래스 조작으로 렌더링 문제 해결!(+컴포넌트 인스턴스화)

--- 길JOB이/src/pages/Onboarding.jsx
+++ 길JOB이/src/pages/Onboarding.jsx
@@ -12,6 +12,49 @@ function Onboarding() {
     navigate("/");
   };
 
+  const regionOptions = [
+    "전체",
+    "서울",
+    "부산",
+    "대구",
+    "인천",
+    "광주",
+    "대전",
+    "울산",
+    "경기",
+    "강원",
+    "충북",
+    "충남",
+    "전북",
+    "전남",
+    "경북",
+    "경남",
+    "제주",
+    "세종",
+  ];
+  const educationOptions = [
+    "고졸 미만",
+    "고교 재학",
+    "고졸 예정",
+    "고교 졸업",
+    "대학 재학",
+    "대졸 예정",
+    "대학 졸업",
+    "석∙박사",
+    "제한없음",
+  ];
+  const jobStatusOptions = [
+    "재직자",
+    "자영업자",
+    "미취업자",
+    "프리랜서",
+    "일용근로자",
+    "(예비)창업자",
+    "단기근로자",
+    "영농종사자",
+    "제한없음",
+  ];
+
   const [age, setAge] = useState();
 
   return (
@@ -25,21 +68,24 @@ function Onboarding() {
           <div className={styles.Onboarding_content}>
             <div className={styles.Onboarding_line}>
               <h4>지역</h4>
-              <div className={styles.dropdown}>
-                <Dropdown />
-              </div>
+              <Dropdown
+                options={regionOptions}
+                defaultOption="지역을 선택해주세요"
+              />
             </div>
             <div className={styles.Onboarding_line}>
               <h4>학력</h4>
-              <div className={styles.dropdown}>
-                <Dropdown />
-              </div>
+              <Dropdown
+                options={educationOptions}
+                defaultOption="학력을 선택해주세요"
+              />
             </div>
             <div className={styles.Onboarding_line}>
               <h4>구직 상태</h4>
-              <div className={styles.dropdown}>
-                <Dropdown />
-              </div>
+              <Dropdown
+                options={jobStatusOptions}
+                defaultOption="구직 상태를 선택해주세요"
+              />
             </div>
             <div
               className={`${styles.Onboarding_line} ${styles.age_container}`}
